d7c4a190d40402e564c14c40cfaf1acf4d0f284a
hvatnir <63882575+hvatnir@users.noreply.github.com>
2020-07-24 22:31:34 +0300

    532
    532

--- created/js/main.js
+++ created/js/main.js
@@ -1,19 +1,16 @@
-var modal = document.getElementById("my_modal");
-var btn = document.getElementById("btn_modal_window");
-var span = document.getElementsByClassName("close")[0];
-
-
-btn.onclick = function () {
-    modal.style.display = "block";
-}
-
-modal.onclick = function () {
-    modal.style.display = "none";
-}
-
-//клик вокруг модального окна
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+var wow = new WOW(
+    {
+      boxClass:     'wow',      // animated element css class (default is wow)
+      animateClass: 'animated', // animation css class (default is animated)
+      offset:       0,          // distance to the element when triggering the animation (default is 0)
+      mobile:       true,       // trigger animations on mobile devices (default is true)
+      live:         true,       // act on asynchronously loaded content (default is true)
+      callback:     function(box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+      },
+      scrollContainer: null,    // optional scroll container selector, otherwise use window,
+      resetAnimation: true,     // reset animation on end (default is true)
     }
-}
+  );
+  wow.init();
