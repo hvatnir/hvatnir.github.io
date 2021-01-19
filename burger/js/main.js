d3566882814b04685d386aca199c6883bddd3308
hvatnir <63882575+hvatnir@users.noreply.github.com>
2021-01-19 22:39:49 +0200

    fdfd331
    f3333

--- burger/js/main.js
+++ burger/js/main.js
@@ -13,3 +13,9 @@ document.addEventListener('click', () => {
 	}, 500)
 });
 
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
